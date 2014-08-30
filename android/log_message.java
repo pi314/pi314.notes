0029286f551025ec8044f1c46fcf7eb4a46d9aa4
Cychih <michael66230@gmail.com>
2014-08-30 22:17:46 +0800

    [android note] Refine log_message.java

--- android/log_message.java
+++ android/log_message.java
@@ -1,12 +1,41 @@
-/* static or not, it depends on situation */
-static private boolean logging = true;
+/* static or not depends on situation */
+public void onCreate (bla) {
+    /* Create session filter */
+    Log.e("TAG", "create session filter");
+}
 
-static private void log_message (String message) {
+static private Handler message_handler;
+static private String handler_tag = "defema.log";
+
+static public void set_handler (Handler message_handler, String handler_tag) {
+    DeFeMa.message_handler = message_handler;
+    DeFeMa.handler_tag = handler_tag;
+}
+
+static private boolean logging = true;
+static private String log_tag = "DeFeMa";
+
+static public void set_log_tag (String log_tag) {
+    DeFeMa.log_tag = log_tag;
+}
+
+static private void notify_message (String message) {
     
     if ( !logging ) return;
     
-    System.out.println("[DeFeMa] " + message);
-    // you can also use Log.d(); or whatever
+    Log.i(log_tag, "[DeFeMa] " + message);
+
+    // Log.v: ?
+    // Log.d: blue
+    // Log.i: green
+    // Log.w: yellow
+    // Log.e: red
     
-    // you can also notify_message();
+    if (message_handler == null) return;
+    
+    Message msgObj = message_handler.obtainMessage();
+    Bundle b = new Bundle();
+    b.putString(handler_tag, message);
+    msgObj.setData(b);
+    message_handler.sendMessage(msgObj);
 }
