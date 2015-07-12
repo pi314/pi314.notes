95d7a22d57079bba9a6e3ef76ec676d54d5ae0b8
Cychih <michael66230@gmail.com>
2015-07-12 13:31:03 +0800

    refine android/Menu/options_menu.java

--- android/Menu/options_menu.java
+++ android/Menu/options_menu.java
@@ -2,6 +2,7 @@
 public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
+    menu.findItem(R.id.action_settings).setVisible(true);
     return true;
 }
 
