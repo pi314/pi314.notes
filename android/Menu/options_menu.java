4d9f0705e5e8bdb75dd47723330ad57aa65bc3e4
Cychih <michael66230@gmail.com>
2015-07-12 13:32:17 +0800

    refine android/Menu/options_menu.java

--- android/Menu/options_menu.java
+++ android/Menu/options_menu.java
@@ -11,9 +11,10 @@ public boolean onOptionsItemSelected(MenuItem item) {
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    if (id == R.id.action_settings) {
+    switch (item.getItemId()) {
+    case R.id.action_settings:
         return true;
+        break;
     }
     return super.onOptionsItemSelected(item);
 }
