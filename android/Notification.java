343b98e7477a5996afeb6a0b7fbdfd43cb6e04f5
Cychih <michael66230@gmail.com>
2015-08-19 01:47:53 +0800

    add android/Notification.java

---
+++ android/Notification.java
@@ -0,0 +1,23 @@
+// To avoid application being opened twice, add
+// android:launchMode="singleTop"
+// to the <activity> element
+public void show_ec_status (boolean status) {
+    show_ec_status_on_monitor_page(status);
+    String text = status ? EasyConnect.EC_HOST : "connecting";
+    NotificationManager notification_manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    NotificationCompat.Builder notification_builder =
+        new NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.ic_launcher)
+        .setContentTitle(C.dm_name)
+        .setContentText(text);
+
+    PendingIntent pending_intent = PendingIntent.getActivity(
+        this,
+        0,
+        new Intent(this, MainActivity.class),
+        PendingIntent.FLAG_UPDATE_CURRENT
+    );
+
+    notification_builder.setContentIntent(pending_intent);
+    notification_manager.notify(NOTIFICATION_ID, notification_builder.build());
+}
