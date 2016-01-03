7f32a90bfd112213ea569a55f3c5523f807f3d50
Cychih <michael66230@gmail.com>
2016-01-03 22:26:10 +0800

    refine android/Handler.java

--- android/Handler.java
+++ android/Handler.java
@@ -1,23 +1,34 @@
 import android.os.Handler;
 
+class Struct {
+    int i;
+    String s;
+}
+
 /* Note: this method will not gernerate a new thread */
 Handler handler = new Handler() {
     public void handleMessage (Message msg) {
-        //int what = msg.getData().getInt("what");
+        // Method 1: use Message.obj
+        Struct st = msg.obj;
+
+        // Method 2: use Bundle
         String message_type = msg.getData().getString("type");
-        if ( message_type.equals("client-socket") ) {
-            String message = msg.getData().getString("message");
-            //...
-        }
+
+        //...
     }
 };
 
 public void notify_message (String type, String message) {
     Message msgObj = handler.obtainMessage();
+    // Method 1: use Message.obj
+    msgObj.obj = new Struct();
+
+    // Method 2: use Bundle
     Bundle b = new Bundle();
     b.putString("type", type);
     b.putString("message", message);
     msgObj.setData(b);
+
     handler.sendMessage(msgObj);
 }
 
