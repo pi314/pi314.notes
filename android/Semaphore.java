0551127b9de79fca2f7eae3b5a037956eacbb342
Cychih <michael66230@gmail.com>
2016-03-25 14:17:06 +0800

    refine android/Semaphore.java

--- android/Semaphore.java
+++ android/Semaphore.java
@@ -1,5 +1,11 @@
 import java.util.concurrent.Semaphore;
 
 Semaphore readlock = new Semaphore(10);
-readlock.acquire();
-readlock.release();
+try {
+    readlock.acquire();
+    readlock.release();
+} catch (InterruptedException) {
+} finally {
+    // no need to release here
+    // because InterruptedException means readlock did not get any permission
+}
