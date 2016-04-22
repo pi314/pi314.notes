7ba613b9aea2f4b06e111744527498a6c9305f5a
Cychih <michael66230@gmail.com>
2016-04-22 17:01:49 +0800

    add android/tmp/initialize-hashmap.java

---
+++ android/tmp/initialize-hashmap.java
@@ -0,0 +1,9 @@
+import java.util.HashMap
+
+class Demo {
+    static final HashMap<String, String[]> demo_map = new HashMap<String, String[]() {{
+        put("key1", new String[]{"value1-1", "value1-2"});
+        put("key2", new String[]{"value2-1"});
+        put("key3", new String[]{"value3-1", "value3-2", "value3-3"});
+    }};
+}
