441388e6bdfd62eb7a2a0d86b7a33c05c7537939
Cychih <michael66230@gmail.com>
2016-04-22 17:12:30 +0800

    refine android/tmp/initialize-hashmap.java

--- android/tmp/initialize-hashmap.java
+++ android/tmp/initialize-hashmap.java
@@ -7,3 +7,9 @@ class Demo {
         put("key3", new String[]{"value3-1", "value3-2", "value3-3"});
     }};
 }
+
+// http://www.softwaregeek.net/2012/12/double-brace-initialization-in-java.html
+// Keyword: double brace initialization
+
+// {{}} 實際上會繼承產生一個 anounymous class，內容是 Constructor
+// 這個方法可能會造成 Memory Leak，以及 .equals() 和 .hashcode() 會失效
