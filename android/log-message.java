166b820e7cb6c1ab1bece6c8fccb88bb18434625
Cychih <michael66230@gmail.com>
2016-04-24 23:01:34 +0800

    refine android/log-message.java

--- android/log-message.java
+++ android/log-message.java
@@ -1,14 +1,7 @@
-static private void logging (String format, Object... args) {
-    logging(String.format(format, args));
+static public void logging (String local_tag, String format, Object... args) {
+    logging(local_tag, String.format(format, args));
 }
 
-static private void logging (String message) {
-    String padding = message.startsWith(" ") || message.startsWith("[") ? "" : " ";
-    Log.i(log_tag, String.format("[%s][%s]%s%s%n", log_tag, local_log_tag, padding, message));
-
-    // Log.v: ?
-    // Log.d: blue
-    // Log.i: green
-    // Log.w: yellow
-    // Log.e: red
+static public void logging (String local_tag, String message) {
+    Log.i(Constants.log_tag, String.format("[%s][%s] %s%n", Constants.log_tag, local_tag, message));
 }
